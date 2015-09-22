0fd403ad4103de5d076e51faa2c9c4fa8a1dba80
Kocheryzhkin Vladimir <vkocherizhkin@gmail.com>
2015-09-22 20:15:35 +0300

    try to link with vlqt5

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,70 @@
 #include <iostream>
 #include <QApplication>
+#include <vlCore/VisualizationLibrary.hpp>
+#include <vlQt5/Qt5Widget.hpp>
 
-#include "mainwindow.h"
+//#include "mainwindow.h"
+#include "rotatingcube.hpp"
 
 using namespace std;
+using namespace vl;
+using namespace vlQt5;
 
 int main(int argc, char *argv[])
 {
+//    QApplication app(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return app.exec();
     QApplication app(argc, argv);
-    MainWindow w;
-    w.show();
-    return app.exec();
-//    cout << "Test" << endl;
-//    return 0;
+
+    /* init Visualization Library */
+    VisualizationLibrary::init();
+
+    /* setup the OpenGL context format */
+    OpenGLContextFormat format;
+    format.setDoubleBuffer(true);
+    format.setRGBABits( 8,8,8,8 );
+    format.setDepthBufferBits(24);
+    format.setStencilBufferBits(8);
+    format.setFullscreen(false);
+    //format.setMultisampleSamples(16);
+    //format.setMultisample(true);
+
+    /* create the applet to be run */
+    ref<Applet> applet = new App_RotatingCube;
+    applet->initialize();
+    /* create a native Qt5 window */
+    ref<vlQt5::Qt5Widget> qt5_window = new vlQt5::Qt5Widget;
+    /* bind the applet so it receives all the GUI events related to the OpenGLContext */
+    qt5_window->addEventListener(applet.get());
+    /* target the window so we can render on it */
+    applet->rendering()->as<Rendering>()->renderer()->setFramebuffer( qt5_window->framebuffer() );
+    /* black background */
+    applet->rendering()->as<Rendering>()->camera()->viewport()->setClearColor( black );
+    /* define the camera position and orientation */
+    vec3 eye    = vec3(0,10,35); // camera position
+    vec3 center = vec3(0,0,0);   // point the camera is looking at
+    vec3 up     = vec3(0,1,0);   // up direction
+    mat4 view_mat = mat4::getLookAt(eye, center, up);
+    applet->rendering()->as<Rendering>()->camera()->setViewMatrix( view_mat );
+    /* Initialize the OpenGL context and window properties */
+    int x = 10;
+    int y = 10;
+    int width = 512;
+    int height= 512;
+    qt5_window->initQt5Widget( "Visualization Library on Qt5 - Rotating Cube", format, NULL, x, y, width, height );
+    /* show the window */
+    qt5_window->show();
+
+    /* run the Win32 message loop */
+    int val = app.exec();
+
+    /* deallocate the window with all the OpenGL resources before shutting down Visualization Library */
+    qt5_window = NULL;
+
+    /* shutdown Visualization Library */
+    VisualizationLibrary::shutdown();
+
+    return val;
 }
